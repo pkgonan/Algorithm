1bb59d3a03d9c72ce849923c04514749bf7a14df
pkgonan <pkgonan1@naver.com>
2016-09-12 02:06:11 +0900

    계단오르기

---
+++ src/BackjoonOnlineJudge/Problem_2579.java
@@ -0,0 +1,28 @@
+package BackjoonOnlineJudge;
+
+import java.util.Scanner;
+
+public class Problem_2579 {
+    int N;
+    int[] stair;
+    int[] dp;
+
+    public void printMaxRecord(){
+        dp[1] = stair[1];
+        dp[2] = stair[1] + stair[2];
+
+        for(int i=3; i<=N; i++)
+            dp[i] = Math.max(stair[i] + dp[i-2] , stair[i] + stair[i-1] + dp[i-3]);
+        System.out.println(dp[N]);
+    }
+
+    public void init(){
+        Scanner scanner = new Scanner(System.in);
+        N = scanner.nextInt();
+        stair = new int[N+1];
+        dp = new int[N+1];
+        for(int i=1; i<=N; i++)
+            stair[i] = scanner.nextInt();
+        scanner.close();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
+import BackjoonOnlineJudge.Problem_2579;
+
 public class Main {
     public static void main(String[] args) {
+        Problem_2579 p = new Problem_2579();
+        p.init();
+        p.printMaxRecord();
     }
 }
