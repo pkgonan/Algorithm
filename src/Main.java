05d4893214113de7ff652ab7fcb2242047f69a5f
pkgonan <pkgonan1@naver.com>
2016-09-12 03:59:35 +0900

    RGB거리

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,4 @@
-import BackjoonOnlineJudge.Problem_2579;
-
 public class Main {
     public static void main(String[] args) {
-        Problem_2579 p = new Problem_2579();
-        p.init();
-        p.printMaxRecord();
     }
 }
