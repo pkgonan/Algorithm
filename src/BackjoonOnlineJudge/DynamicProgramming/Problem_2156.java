0d179b7d72a8999532e578259bc78e560e5d6e90
pkgonan <pkgonan1@naver.com>
2016-09-12 21:58:20 +0900

    포도주 시식

---
+++ src/BackjoonOnlineJudge/DynamicProgramming/Problem_2156.java
@@ -0,0 +1,34 @@
+package BackjoonOnlineJudge.DynamicProgramming;
+
+import java.util.Scanner;
+
+public class Problem_2156 {
+    int N;
+    int[] dp;
+    int[] cup;
+
+    public void printResult(){
+        dp[1] = cup[1];
+        if(N>=2) {
+            dp[2] = cup[1] + cup[2];
+            for (int i = 3; i <= N; i++)
+                dp[i] = MAX(cup[i] + cup[i - 1] + dp[i - 3], cup[i] + dp[i - 2], dp[i - 1]);
+        }
+        System.out.println(dp[N]);
+    }
+
+    private int MAX(int first, int second, int third){
+        int max = Math.max(first, second);
+        return Math.max(max, third);
+    }
+
+    public void init(){
+        Scanner scanner = new Scanner(System.in);
+        N = scanner.nextInt();
+        dp = new int[N+1];
+        cup = new int[N+1];
+        for(int i=1; i<=N; i++)
+            cup[i] = scanner.nextInt();
+        scanner.close();
+    }
+}
