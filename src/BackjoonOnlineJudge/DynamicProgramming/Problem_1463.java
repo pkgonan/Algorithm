6c66926c04f09949c92fc504f5455f13441c0d0f
pkgonan <pkgonan1@naver.com>
2016-09-12 20:09:29 +0900

    1로 만들기

--- src/BackjoonOnlineJudge/DynamicProgramming/Problem_1463.java
+++ src/BackjoonOnlineJudge/DynamicProgramming/Problem_1463.java
@@ -10,8 +10,13 @@ public class Problem_1463 {
         if (N == 1)
             System.out.println(0);
         else {
-            for(int i=2; i<=N; i++)
-                BUCKET[i] = MIN(BUCKET[i - 1] + 1 , (i % 2 == 0)? BUCKET[i / 2] + 1 : Integer.MAX_VALUE , (i % 3 == 0)? BUCKET[i / 3] + 1 : Integer.MAX_VALUE);
+            for(int i=2; i<=N; i++) {
+                BUCKET[i] = BUCKET[i - 1] + 1;
+                if(i%2 == 0 && BUCKET[i] > BUCKET[i/2] + 1)
+                    BUCKET[i] = BUCKET[i/2] + 1;
+                if(i%3 == 0 && BUCKET[i] > BUCKET[i/3] + 1)
+                    BUCKET[i] = BUCKET[i/3] + 1;
+            }
             System.out.println(BUCKET[N]);
         }
     }
