78933cf9fae193b9c036ca76dc4c8b0c35948a58
pkgonan <pkgonan1@naver.com>
2016-09-17 01:03:19 +0900

    내리막 길

--- src/BackjoonOnlineJudge/DynamicProgramming/Problem_1520.java
+++ src/BackjoonOnlineJudge/DynamicProgramming/Problem_1520.java
@@ -16,11 +16,11 @@ public class Problem_1520 {
             return 1;
         if (visited[i][j] != 0)
             return visited[i][j];
-        if (i - 1 >= 0 && arr[i][j] > arr[i - 1][j])
+        if (i - 1 >= 1 && arr[i][j] > arr[i - 1][j])
             visited[i][j] += getValueByRecursion(i - 1, j);
         if (i + 1 <= N && arr[i][j] > arr[i + 1][j])
             visited[i][j] += getValueByRecursion(i + 1, j);
-        if (j - 1 >= 0 && arr[i][j] > arr[i][j - 1])
+        if (j - 1 >= 1 && arr[i][j] > arr[i][j - 1])
             visited[i][j] += getValueByRecursion(i, j - 1);
         if (j + 1 <= M && arr[i][j] > arr[i][j + 1])
             visited[i][j] += getValueByRecursion(i, j + 1);
