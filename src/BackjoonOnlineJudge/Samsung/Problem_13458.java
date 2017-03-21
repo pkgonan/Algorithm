d17ef821be7287439f101ee85adf1a1188390f5d
pkgonan <pkgonan1@naver.com>
2017-03-22 00:13:16 +0900

    시험 감독

---
+++ src/BackjoonOnlineJudge/Samsung/Problem_13458.java
@@ -0,0 +1,27 @@
+package BackjoonOnlineJudge.Samsung;
+
+import java.util.Scanner;
+
+public class Problem_13458 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[1000000];
+        int N,B,C;
+        long RESULT = 0;
+
+        N = sc.nextInt();
+        for(int i=0; i<N; i++)
+            arr[i] = sc.nextInt();
+        B = sc.nextInt();
+        C = sc.nextInt();
+
+        for(int i=0; i<N; i++) {
+            RESULT++;
+            if(arr[i]-B > 0) {
+                RESULT += (arr[i]-B)%C != 0? 1 : 0;
+                RESULT += (arr[i]-B)/C;
+            }
+        }
+        System.out.println(RESULT);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
+import BackjoonOnlineJudge.DP.Problem_2602;
+
 public class Main {
     public static void main(String[] args) {
+        Problem_2602 p = new Problem_2602();
+        p.init();
+        p.printResult();
+
     }
 }
