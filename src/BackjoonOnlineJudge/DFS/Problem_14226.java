ef1d3ee81be4e01333203798b88840e44030dbf1
pkgonan <pkgonan1@naver.com>
2017-04-09 07:04:59 +0900

    이모티콘

--- src/BackjoonOnlineJudge/DFS/Problem_14226.java
+++ src/BackjoonOnlineJudge/DFS/Problem_14226.java
@@ -26,20 +26,17 @@ public class Problem_14226 {
             if(now.M == S) MIN = Math.min(MIN, now.T);
 
             if(now.M < S) {
-                // 1번 연산 : 현재 화면의 갯수를 클립보드에 저장
                 if(now.T+1 <= S) {
                     if(now.M != now.C && !visited[now.M][now.M]) {
                         Q.add(new A(now.M, now.M, now.T+1));
                         visited[now.M][now.M] = true;
                     }
 
-                    // 2번 연산 : 클립보드의 갯수를 모니터에 더해
-                    if (now.C != 0 && now.M+now.C <= S && !visited[now.M+now.C][now.C]) { // 클립보드가 비어있지 않을때 붙여넣기 가능
+                    if (now.C != 0 && now.M+now.C <= S && !visited[now.M+now.C][now.C]) {
                         Q.add(new A(now.M+now.C, now.C, now.T+1));
                         visited[now.M+now.C][now.C] = true;
                     }
 
-                    // 3번 연산 : 화면값을 감소시켜
                     if (now.M-1 >= 1 && now.T+1 < MIN && !visited[now.M-1][now.C]) {
                         Q.add(new A(now.M-1, now.C, now.T+1));
                         visited[now.M-1][now.C] = true;
@@ -48,7 +45,6 @@ public class Problem_14226 {
             }
             else {
                 if(now.T+1 <= S) {
-                    // 3번 연산 : 화면값을 감소시켜
                     if (now.M-1 >= 1 && now.T+1 < MIN && !visited[now.M-1][now.C]) {
                         Q.add(new A(now.M-1, now.C, now.T+1));
                         visited[now.M-1][now.C] = true;
