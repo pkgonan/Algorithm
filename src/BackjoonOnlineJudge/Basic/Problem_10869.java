fc6be08f8312eb61284b80ffd92104f4adab20c5
pkgonan <pkgonan1@naver.com>
2017-04-15 04:05:21 +0900

    사칙연산

---
+++ src/BackjoonOnlineJudge/Basic/Problem_10869.java
@@ -0,0 +1,45 @@
+package BackjoonOnlineJudge.Basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem_10869 {
+    static int A,B;
+
+    public static void main(String[] args) throws IOException {
+        init();
+        printAdd();
+        printSub();
+        printMul();
+        printDiv();
+        printMod();
+    }
+
+    static void printAdd(){
+        System.out.println(A+B);
+    }
+
+    static void printSub(){
+        System.out.println(A-B);
+    }
+
+    static void printMul(){
+        System.out.println(A*B);
+    }
+
+    static void printDiv(){
+        System.out.println(A/B);
+    }
+
+    static void printMod(){
+        System.out.println(A%B);
+    }
+
+    static void init() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        A = Integer.parseInt(s[0]);
+        B = Integer.parseInt(s[1]);
+    }
+}
