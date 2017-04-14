c45ca71ec2093e19f0a05bcbd16452e046150537
pkgonan <pkgonan1@naver.com>
2017-04-15 03:55:02 +0900

    A*B

---
+++ src/BackjoonOnlineJudge/Basic/Problem_10998.java
@@ -0,0 +1,13 @@
+package BackjoonOnlineJudge.Basic;
+
+import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
+
+public class Problem_10998 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        System.out.println(Integer.parseInt(s[0]) * Integer.parseInt(s[1]));
+    }
+}
