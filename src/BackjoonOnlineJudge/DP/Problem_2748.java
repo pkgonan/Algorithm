e97c41d175148708f8502686171541638e0dc29d
pkgonan <pkgonan1@naver.com>
2017-04-15 03:12:03 +0900

    피보나치 수 2

---
+++ src/BackjoonOnlineJudge/DP/Problem_2748.java
@@ -0,0 +1,33 @@
+package BackjoonOnlineJudge.DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem_2748 {
+    static int N;
+    static long[] DP;
+
+    public static void main(String[] args) throws IOException{
+        init();
+        start();
+    }
+
+    static void start(){
+        DP[0] = 0;
+
+        if(N==0) System.out.println(0);
+        else {
+            DP[1] = 1;
+            for (int i = 2; i <= N; i++)
+                DP[i] = DP[i - 1] + DP[i - 2];
+            System.out.println(DP[N]);
+        }
+    }
+
+    static void init() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        DP = new long[N+1];
+    }
+}
