7c8b70b51502311be83800bb8827145bd743b955
pkgonan <pkgonan1@naver.com>
2017-04-20 01:04:06 +0900

    단어의 개수

---
+++ src/BackjoonOnlineJudge/Basic/Problem_1152.java
@@ -0,0 +1,18 @@
+package BackjoonOnlineJudge.Basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem_1152 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().trim().split(" ");
+        int cnt = 0;
+
+        for(int i=0; i<s.length; i++)
+            if(!s[i].equals("")) cnt++;
+
+        System.out.println(cnt);
+    }
+}
