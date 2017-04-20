c4eb8315ebcf3b5fac0a70dd077ae5c7e8798731
pkgonan <pkgonan1@naver.com>
2017-04-21 00:15:12 +0900

    알파벳 개수

---
+++ src/BackjoonOnlineJudge/String/Problem_10808.java
@@ -0,0 +1,21 @@
+package BackjoonOnlineJudge.String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem_10808 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int[] cnt = new int[257];
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<s.length(); i++)
+            cnt[s.charAt(i)]++;
+
+        for(int i='a'; i<='z'; i++)
+            sb.append(cnt[i] + " ");
+        System.out.println(sb);
+    }
+}
