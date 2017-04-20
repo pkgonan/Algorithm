f1dff595a3c005546f562fd871a021c246198fec
pkgonan <pkgonan1@naver.com>
2017-04-21 00:16:27 +0900

    문자열 반복

---
+++ src/BackjoonOnlineJudge/String/Problem_2675.java
@@ -0,0 +1,24 @@
+package BackjoonOnlineJudge.String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem_2675 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<T; i++){
+            String[] s = br.readLine().split(" ");
+            StringBuilder sb = new StringBuilder();
+            int R = Integer.parseInt(s[0]);
+            String S = s[1];
+
+            for(int j=0; j<S.length(); j++)
+                for(int k=0; k<R; k++)
+                    sb.append(S.charAt(j));
+            System.out.println(sb);
+        }
+    }
+}
