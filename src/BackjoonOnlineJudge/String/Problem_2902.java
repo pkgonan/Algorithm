980a801f4bd76ff0cc7077fddb3fe3f915f8b9ef
pkgonan <pkgonan1@naver.com>
2017-04-21 00:21:18 +0900

    KMP는 왜 KMP일까?

---
+++ src/BackjoonOnlineJudge/String/Problem_2902.java
@@ -0,0 +1,17 @@
+package BackjoonOnlineJudge.String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem_2902 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split("-");
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<s.length; i++)
+            sb.append(s[i].charAt(0));
+        System.out.println(sb);
+    }
+}
