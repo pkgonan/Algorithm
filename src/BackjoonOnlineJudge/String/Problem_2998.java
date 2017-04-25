78d6d2c137defbb727283e3119a1f0c59ffa8145
pkgonan <pkgonan1@naver.com>
2017-04-25 22:39:28 +0900

    8진수

---
+++ src/BackjoonOnlineJudge/String/Problem_2998.java
@@ -0,0 +1,29 @@
+package BackjoonOnlineJudge.String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem_2998 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder(br.readLine());
+        StringBuilder result = new StringBuilder();
+        while(sb.length() % 3 != 0)
+            sb.insert(0, '0');
+        while(sb.length() != 0){
+            String tmp = sb.substring(sb.length()-3, sb.length());
+            sb.delete(sb.length()-3, sb.length());
+
+            if(tmp.equals("000")) result.insert(0, '0');
+            else if(tmp.equals("001")) result.insert(0, '1');
+            else if(tmp.equals("010")) result.insert(0, '2');
+            else if(tmp.equals("011")) result.insert(0, '3');
+            else if(tmp.equals("100")) result.insert(0, '4');
+            else if(tmp.equals("101")) result.insert(0, '5');
+            else if(tmp.equals("110")) result.insert(0, '6');
+            else result.insert(0, '7');
+        }
+        System.out.println(result);
+    }
+}
