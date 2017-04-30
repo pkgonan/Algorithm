fb466500cd9ecf9d8bcb836381d215635f582c3a
pkgonan <pkgonan1@naver.com>
2017-04-30 21:44:12 +0900

    초콜릿 자르기

---
+++ src/BackjoonOnlineJudge/DP/Problem_2163.java
@@ -0,0 +1,23 @@
+package BackjoonOnlineJudge.DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem_2163 {
+    static int NM;
+
+    public static void main(String[] args) throws IOException{
+        init();
+        System.out.println(NM-1);
+    }
+
+    static void init() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        NM = N*M;
+    }
+}
