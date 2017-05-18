52d5f962d4285b804d20a92de1c954fc2b26bf9b
pkgonan <pkgonan1@naver.com>
2017-05-19 00:16:31 +0900

    큰 수 A+B

---
+++ src/BackjoonOnlineJudge/Common/Basic/Problem_10757.java
@@ -0,0 +1,12 @@
+package BackjoonOnlineJudge.Common.Basic;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Problem_10757 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        BigInteger sum = sc.nextBigInteger().add(sc.nextBigInteger());
+        System.out.println(sum);
+    }
+}
