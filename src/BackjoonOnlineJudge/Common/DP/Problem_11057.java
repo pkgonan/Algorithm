57680e838f103cf7bba8344e2a70d1324c3aeb0e
pkgonan <pkgonan1@naver.com>
2017-05-19 21:33:02 +0900

    오르막 수

--- src/BackjoonOnlineJudge/Common/DP/Problem_11057.java
+++ src/BackjoonOnlineJudge/Common/DP/Problem_11057.java
@@ -4,33 +4,20 @@ import java.util.Scanner;
 
 public class Problem_11057 {
     final static int MOD = 10007;
-    int N;
-    int[][] M;
-    int sum;
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[][] dp = new int[N+1][10];
 
-    public void getResult(){
-        for (int i=0; i<10; i++)
-            M[1][i] = 1;
-
+        for(int i=0; i<10; i++)
+            dp[1][i]=1;
         for(int i=2; i<=N; i++)
             for(int j=0; j<10; j++)
-                for(int k=j; k<10; k++)
-                    M[i][j] += M[i-1][k] % MOD;
-        System.out.println(getSum());
-    }
-
-    private int getSum(){
+                for(int k=0; k<=j; k++)
+                    dp[i][j] += (dp[i-1][k] % MOD);
+        int sum = 0;
         for(int i=0; i<10; i++)
-            sum += M[N][i];
-        return sum;
+            sum += dp[N][i];
+        System.out.println(sum % MOD);
     }
-
-    public void init(){
-        Scanner scanner = new Scanner(System.in);
-        M = new int[1001][10];
-        sum = 0;
-        N = scanner.nextInt();
-        scanner.close();
-    }
-
 }
