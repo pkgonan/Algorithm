e6bb25691d7b2a985b83048c93c3ae60d88579cb
pkgonan <pkgonan1@naver.com>
2017-07-03 22:37:26 +0900

    그대로 출력하기 2

---
+++ src/BackjoonOnlineJudge/Common/Basic/Problem_11719.java
@@ -0,0 +1,11 @@
+package BackjoonOnlineJudge.Common.Basic;
+
+import java.util.Scanner;
+
+public class Problem_11719 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine())
+            System.out.println(sc.nextLine());
+    }
+}
