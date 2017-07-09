dca91811ce989a1a17e59c0ed45bf97e4e48f638
pkgonan <pkgonan1@naver.com>
2017-07-09 22:26:43 +0900

    합

---
+++ src/BackjoonOnlineJudge/Common/Basic/Problem_8393.java
@@ -0,0 +1,10 @@
+package BackjoonOnlineJudge.Common.Basic;
+
+import java.util.Scanner;
+
+public class Problem_8393 {
+    public static void main(String[] args){
+        int n = new Scanner(System.in).nextInt();
+        System.out.println(n * (n+1) / 2);
+    }
+}
