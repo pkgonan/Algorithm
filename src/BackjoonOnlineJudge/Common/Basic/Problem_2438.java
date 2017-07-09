0365f5713e6382255de4e46f03936badf7bf40e9
pkgonan <pkgonan1@naver.com>
2017-07-09 21:34:16 +0900

    별찍기 - 1

---
+++ src/BackjoonOnlineJudge/Common/Basic/Problem_2438.java
@@ -0,0 +1,14 @@
+package BackjoonOnlineJudge.Common.Basic;
+
+import java.util.Scanner;
+
+public class Problem_2438 {
+    public static void main(String[] args){
+        int N = new Scanner(System.in).nextInt();
+        for(int i=1; i<=N; i++) {
+            for (int j = 0; j < i; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
