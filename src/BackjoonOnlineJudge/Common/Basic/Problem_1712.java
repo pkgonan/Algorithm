13611292d523d17889b57e8ef7ca39c38fc16f15
pkgonan <pkgonan1@naver.com>
2017-07-16 20:33:43 +0900

    손익분기점

--- src/BackjoonOnlineJudge/Common/Basic/Problem_1712.java
+++ src/BackjoonOnlineJudge/Common/Basic/Problem_1712.java
@@ -5,19 +5,11 @@ import java.util.Scanner;
 public class Problem_1712 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        long A = sc.nextLong();
-        long B = sc.nextLong();
-        long C = sc.nextLong();
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
 
-        System.out.println(getResult(A, B, C));
-    }
-
-    static int getResult(long A, long B, long C){
-        int cnt = 0;
-        for (int i=1; i<Integer.MAX_VALUE; i++) {
-            cnt++;
-            if (A + B * i < C * i) return cnt;
-        }
-        return -1;
+        if(B >= C) System.out.println(-1);
+        else System.out.println(A / (C-B) + 1);
     }
 }
